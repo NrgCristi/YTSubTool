8731328473b5e2a477f23baccd5797cf454c02f0
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-08-25 11:48:33 +0300

    Add files via upload

---
+++ YTSubTool/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YTSubTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("500 Subscribers Added");
+            System.Diagnostics.Process.Start("textBox1.Text");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1000 Subscribers Added");
+            System.Diagnostics.Process.Start("textBox1.Text");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("10000 Subscribers Added");
+            System.Diagnostics.Process.Start("textBox1.Text");
+        }
+    }
+}
